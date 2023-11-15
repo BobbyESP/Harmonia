3cf951eeab274cf14095afaf3014d0e7e28f20c2
Kyant0 <kyant2021@outlook.com>
2023-11-15 15:37:46 +0800

    [ui][new] icon button

---
+++ ui/src/main/java/com/kyant/ui/IconButton.kt
@@ -0,0 +1,58 @@
+package com.kyant.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.kyant.ui.ripple.rememberRipple
+import com.kyant.ui.theme.ColorToken
+import com.kyant.ui.theme.LocalColorToken
+
+@Composable
+fun IconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colorToken: ColorToken = LocalColorToken.current.copy(color = Color.Transparent),
+    shape: Shape = CircleShape,
+    size: Dp = 40.dp,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(shape)
+            .background(colorToken.color)
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+                role = Role.Button,
+                interactionSource = interactionSource,
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = size / 2
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        CompositionLocalProvider(
+            LocalContentColor provides colorToken.contentColor,
+            content = content
+        )
+    }
+}
