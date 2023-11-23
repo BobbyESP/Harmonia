3f4f082e669a166d3cb92398895699d2c10cbb5c
Kyant0 <kyant2021@outlook.com>
2023-11-23 21:19:19 +0800

    [ui] Add `elevation` system

---
+++ ui/src/main/java/com/kyant/ui/style/elevation/Elevation.kt
@@ -0,0 +1,73 @@
+package com.kyant.ui.style.elevation
+
+import androidx.compose.ui.unit.dp
+
+object Elevation {
+
+    /**
+     * - Assist Chip (Flat)
+     * - Carousel Item
+     * - Filled Button
+     * - Filled Tonal Button
+     * - Filled Card
+     * - Filter Chip (Flat)
+     * - Full Screen Dialog
+     * - List Item
+     * - Input Chip
+     * - Navigation Rail
+     * - Primary Navigation Tabs
+     * - Secondary Navigation Tabs
+     * - Outlined Card
+     * - Side Sheet (Docked)
+     * - Slider (Track)
+     * - Suggestion Chip (Flat)
+     * - Top App Bar
+     */
+    val Level0 = 0.dp
+
+    /**
+     * - Assist Chip (Elevated)
+     * - Banner
+     * - Bottom Sheet (Modal)
+     * - Elevated Button
+     * - Elevated Card
+     * - Extended FAB (Lowered)
+     * - FAB (Lowered)
+     * - Filter Chip (Elevated)
+     * - Navigation Drawer (Modal)
+     * - Side Sheet (Modal)
+     * - Slider (Handle)
+     * - Suggestion Chip (Elevated)
+     */
+    val Level1 = 1.dp
+
+    /**
+     * - Autocomplete Menu
+     * - Bottom App Bar
+     * - Dropdown Menu
+     * - Menu
+     * - Navigation Bar
+     * - Select Menu
+     * - Rich Tooltip
+     * - Top App Bar (Scrolled)
+     */
+    val Level2 = 3.dp
+
+    /**
+     * - FAB
+     * - Extended FAB
+     * - Modal Date Picker
+     * - Docked Date Picker
+     * - Modal Date Input
+     * - Dialog
+     * - Search Bar
+     * - Search View
+     * - Time Picker
+     * - Time Input
+     */
+    val Level3 = 6.dp
+
+    val Level4 = 8.dp
+
+    val Level5 = 12.dp
+}
