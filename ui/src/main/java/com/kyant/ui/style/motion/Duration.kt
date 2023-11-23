7ff89078e4c4872ece6aa0468d006a00c17e246d
Kyant0 <kyant2021@outlook.com>
2023-11-23 21:25:02 +0800

    [ui] Refactor `motion` system

--- ui/src/main/java/com/kyant/ui/style/motion/Duration.kt
+++ ui/src/main/java/com/kyant/ui/style/motion/Duration.kt
@@ -1,23 +1,23 @@
 package com.kyant.ui.style.motion
 
 object Duration {
-    const val SHORT_1 = 50L
-    const val SHORT_2 = 100L
-    const val SHORT_3 = 150L
-    const val SHORT_4 = 200L
+    const val SHORT_1 = 50
+    const val SHORT_2 = 100
+    const val SHORT_3 = 150
+    const val SHORT_4 = 200
 
-    const val MEDIUM_1 = 250L
-    const val MEDIUM_2 = 300L
-    const val MEDIUM_3 = 350L
-    const val MEDIUM_4 = 400L
+    const val MEDIUM_1 = 250
+    const val MEDIUM_2 = 300
+    const val MEDIUM_3 = 350
+    const val MEDIUM_4 = 400
 
-    const val LONG_1 = 450L
-    const val LONG_2 = 500L
-    const val LONG_3 = 550L
-    const val LONG_4 = 600L
+    const val LONG_1 = 450
+    const val LONG_2 = 500
+    const val LONG_3 = 550
+    const val LONG_4 = 600
 
-    const val EXTRA_LONG_1 = 700L
-    const val EXTRA_LONG_2 = 800L
-    const val EXTRA_LONG_3 = 900L
-    const val EXTRA_LONG_4 = 1000L
+    const val EXTRA_LONG_1 = 700
+    const val EXTRA_LONG_2 = 800
+    const val EXTRA_LONG_3 = 900
+    const val EXTRA_LONG_4 = 1000
 }
--- ui/src/main/java/com/kyant/ui/style/motion/Easing.kt
+++ ui/src/main/java/com/kyant/ui/style/motion/Easing.kt
@@ -2,16 +2,19 @@ package com.kyant.ui.style.motion
 
 import com.kyant.ui.animation.CubicBezierEasing
 
-val Emphasized = CubicBezierEasing(0.2f, 0f, 0f, 1f)
+object Easing {
 
-val EmphasizedAccelerate = CubicBezierEasing(0.3f, 0f, 0.8f, 0.15f)
+    val Emphasized = CubicBezierEasing(0.2f, 0f, 0f, 1f)
 
-val EmphasizedDecelerate = CubicBezierEasing(0.05f, 0.7f, 0.1f, 1f)
+    val EmphasizedAccelerate = CubicBezierEasing(0.3f, 0f, 0.8f, 0.15f)
 
-val Standard = CubicBezierEasing(0.2f, 0f, 0f, 1f)
+    val EmphasizedDecelerate = CubicBezierEasing(0.05f, 0.7f, 0.1f, 1f)
 
-val StandardAccelerate = CubicBezierEasing(0.3f, 0f, 1f, 1f)
+    val Standard = CubicBezierEasing(0.2f, 0f, 0f, 1f)
 
-val StandardDecelerate = CubicBezierEasing(0f, 0f, 0f, 1f)
+    val StandardAccelerate = CubicBezierEasing(0.3f, 0f, 1f, 1f)
 
-val Linear = CubicBezierEasing(0f, 0f, 1f, 1f)
+    val StandardDecelerate = CubicBezierEasing(0f, 0f, 0f, 1f)
+
+    val Linear = CubicBezierEasing(0f, 0f, 1f, 1f)
+}
