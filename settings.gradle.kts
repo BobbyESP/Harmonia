d708b33e466408ddf1497279f907c6f2e08b6d97
Kyant0 <kyant2021@outlook.com>
2023-11-26 14:32:44 +0800

    [deps] Using Androidx snapshot maven 11142388 Update taglib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+        maven("https://androidx.dev/snapshots/builds/11142388/artifacts/repository")
         maven("https://androidx.dev/storage/compose-compiler/repository")
     }
 }
