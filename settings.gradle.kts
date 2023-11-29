a468c2088dfe77d57bf1432edfcffb9e7be4d17d
Kyant0 <kyant2021@outlook.com>
2023-11-29 11:36:47 +0800

    [deps] Androidx snapshot 11153387

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        maven("https://androidx.dev/snapshots/builds/11148808/artifacts/repository")
+        maven("https://androidx.dev/snapshots/builds/11153387/artifacts/repository")
         maven("https://androidx.dev/storage/compose-compiler/repository")
     }
 }
