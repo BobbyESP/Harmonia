79bdde14dd9173932be4b65f8763c0bb6c3d9031
Kyant0 <kyant2021@outlook.com>
2023-12-20 18:49:22 +0800

    [deps] Move Androidx snapshot build id to gradle.properties and bump it to 11240445

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 pluginManagement {
     repositories {
         google {
@@ -12,12 +14,20 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    val gradleProperties = Properties()
+    val gradlePropertiesFile = File(rootProject.projectDir, "gradle.properties")
+    if (gradlePropertiesFile.exists()) {
+        gradleProperties.load(gradlePropertiesFile.inputStream())
+    }
+    val androidxSnapshotBuildId = gradleProperties.getProperty("androidx.snapshotBuildId")
+        ?: throw GradleException("Androidx snapshot build id must be defined in gradle.properties.")
+
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        maven("https://androidx.dev/snapshots/builds/11233998/artifacts/repository")
+        maven("https://androidx.dev/snapshots/builds/$androidxSnapshotBuildId/artifacts/repository")
         maven("https://androidx.dev/storage/compose-compiler/repository")
     }
 }
